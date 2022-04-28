2ecc47756b5450a57d98f3e67d6e9fc6e8b8cc15
William Kee <keewill57@gmail.com>
2022-04-28 14:21:33 -0700

    Insert statements to add data to all tables

---
+++ coffee_inserts.sql
@@ -0,0 +1,23 @@
+INSERT INTO COFFEE_SHOP
+VALUES 
+    (1, "KBrew", "Knoxville", "TN"),
+    (2, "Temple Coffee", "Sacramento", "CA"),
+    (3, "Cool Beans", "Columbia", "SC");
+
+INSERT INTO SUPPLIER
+VALUES 
+    (1, "D's Beans", "United States", "Moby Dick", "greatwhale@gmail.com"),
+    (2, "GoGoMoMo", "United Kingdom", "Nick Carraway", "americandream@yahoo.com"),
+    (3, "Lord of the Beans", "New Zealand", "Henry Skrimshander", "shortstop@sc.rr.com");
+    
+INSERT INTO EMPLOYEE
+VALUES 
+    (1, "Ernest", "Hemingway", "2016-02-04", "Barista", 1),
+    (2, "Herman", "Melville", "2018-12-15", "Shift Supervisor", 2),
+    (3, "Virginia", "Woolf", "2020-06-22", "Barista", 3);
+    
+INSERT INTO COFFEE
+VALUES 
+    (1, 3, 2, "Headrush", 32.30),
+    (2, 1, 1, "Mindbender", 93.43),
+    (3, 2, 3, "Gormond", 75.23);
